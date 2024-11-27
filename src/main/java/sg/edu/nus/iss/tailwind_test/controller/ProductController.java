09cc7b19d60d78fc3f2ed745cbc88be2121fb166
Claaarree <clare.fh@hotmail.com>
2024-11-27 13:53:44 +0800

    resolved some errors

--- src/main/java/sg/edu/nus/iss/tailwind_test/controller/ProductController.java
+++ src/main/java/sg/edu/nus/iss/tailwind_test/controller/ProductController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import sg.edu.nus.iss.tailwind_test.model.Product;
